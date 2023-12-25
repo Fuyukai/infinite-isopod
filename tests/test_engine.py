7e50daa7711abf991873fb4d827dcb88df706bcb
Lura Skye <l@veriny.tf>
2023-12-25 21:37:53 +0000

    add handful of more tests

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -1,9 +1,11 @@
+import contextlib
 from collections.abc import AsyncGenerator
 
 import pytest
 from infipod.engine import IsopodPool, SchemaLoadFailedError, spawn_isopods_from_pool
 from infipod.schema import Column, TableSchema
 from infipod.types import Int4Type, TextType
+from pg_purepy import MissingRowError
 from pg_purepy.pool import PooledDatabaseInterface
 from trio.testing import RaisesGroup
 
@@ -40,9 +42,22 @@ async def test_loading_engine_without_existing_table(postgresql: PooledDatabaseI
         await spawn_isopods_from_pool(postgresql, [ExampleTable])
 
 
-async def test_loading_engine_with_missing_column(postgresql: PooledDatabaseInterface):
+async def test_loading_non_strict(postgresql: PooledDatabaseInterface):
+    await spawn_isopods_from_pool(postgresql, [ExampleTable], strict_schema_loading=False)
+
+
+async def test_missing_column_server_side(postgresql: PooledDatabaseInterface):
     await postgresql.execute(
         "CREATE TEMPORARY TABLE example_table (id INT4 GENERATED ALWAYS AS IDENTITY);"
+    )
+
+    with RaisesGroup(SchemaLoadFailedError):
+        await spawn_isopods_from_pool(postgresql, [ExampleTable])
+
+
+async def test_missing_column_client_side(postgresql: PooledDatabaseInterface):
+    await postgresql.execute(
+        "CREATE TEMPORARY TABLE example_table (id INT4 PRIMARY KEY, field TEXT, missing TEXT)"
     )
 
     with RaisesGroup(SchemaLoadFailedError):
@@ -78,3 +93,15 @@ async def test_indexing_using_index(engine: IsopodPool):
     row = await engine.fetch_one("SELECT id, field FROM example_table;")
     assert row[0] >= 0
     assert row[1] == "04kamuidrone"
+
+
+async def test_checking_out_connection(engine: IsopodPool):
+    # ensures the checked out connection is actually in a transaction
+
+    with contextlib.suppress(ValueError):
+        async with engine.checkout_isopod() as isopod:
+            await isopod.execute("INSERT INTO example_table (field) VALUES ('test');")
+            raise ValueError
+
+    with pytest.raises(MissingRowError):
+        await engine.fetch_one("SELECT * FROM example_table;")
